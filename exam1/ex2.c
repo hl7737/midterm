8a55b341abce822b3e65a194335f8d1d73b4f4b9
hl7737 <850567274@qq.com>
2019-11-21 23:30:16 -0800

    added a ex2.c file

---
+++ exam1/ex2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int max=-1;
+    do{
+        int a;
+        //d=getchar();
+        if(a<0)break;
+        getchar();
+        if(max==-1)
+        max=a;
+        if(max<a)
+        max=a;
+    }
+    while(1);
+    prinf("max=%d\n",max);
+    return 0;
+}
