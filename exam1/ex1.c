c5cf2d8a6bfffdd9eefaf99faae70207b908bc97
hl7737 <850567274@qq.com>
2019-11-22 00:34:58 -0800

    added a ex1.c file

---
+++ exam1/ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<ctype.h>
+int main()
+{
+    int k,num=1,alpha=0;
+    char str[50],c;
+    gets (str);
+    puts("请输入一个长度不超过50的字符串:\n");
+    for(k=0,str[k]!="\0";k++;)
+    {
+    if((str[k]))alpha++;}
+    printf("输出单词数:%d\n",num);
+    return 0;
+}
